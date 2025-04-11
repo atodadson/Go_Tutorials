db357ac717d75356d31a49423d4cdbc35eb03c7a
atodadson <benjamindadson32@gmail.com>
2025-04-11 12:54:50 +0000

    Read JSON functions

--- readinputs/readinput.go
+++ readinputs/readinput.go
@@ -2,6 +2,7 @@ package readinputs
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 )
@@ -23,23 +24,42 @@ func ReadLine(message string) (string, error) {
 // ReadTxt reads the content of a textfile and returns the whole content as a string
 // It takes the file directory as a string and returns
 func ReadTxt(filename string) (string, error) {
+	// Open the file
 	file, err := os.Open(filename)
 	if err != nil {
 		return "", err
 	}
 
+	// Scanner for scanning the file content
 	scanner := bufio.NewScanner(file)
 	output := ""
+
+	// Scan the file token by token
 	for scanner.Scan() {
-		output = output + scanner.Text() + "\n"
+		output = output + scanner.Text() + "\n" // Seperates tokens by end of line
 	}
 	if err := scanner.Err(); err != nil {
 		return output, err
 	}
-	return output, nil
+	return output, nil // return string and nil when successful
 
 }
 
-// func ReadTxt(filepath string) string {
-// 	return os.FileInfo(filepath)
-// }
+// ReadJson reads the content of a JSON file.
+// Parameters: filename (string), struct_ (pointer to a struct for unmarshalling).
+// Returns: error if it occurs.
+func ReadJson(filename string, struct_ any) error {
+	// Read the file's content.
+	byteList, err := os.ReadFile(filename)
+	if err != nil {
+		return err // Return file read error.
+	}
+
+	// Unmarshal JSON into the provided struct pointer.
+	unmarshalErr := json.Unmarshal(byteList, struct_)
+	if unmarshalErr != nil {
+		return unmarshalErr // Return unmarshalling error.
+	}
+
+	return nil // Success, no errors.
+}
