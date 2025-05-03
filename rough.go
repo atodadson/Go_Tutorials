85c98cc0fe628f550c6f67f8650e702f70a7f6bc
atodadson <benjamindadson32@gmail.com>
2025-05-03 14:14:56 +0000

    Testing get

--- rough.go
+++ rough.go
@@ -2,60 +2,24 @@ package main
 
 import (
     "fmt"
-	// "go_tuts/readinputs"
-	// "os"
-	"strconv"
-	"strings"
-	"time"
+    "net/http"
+    "github.com/gin-gonic/gin"
 )
 
-func genNumbers() {
-    for i := 0; i < 100; i++ {
-        fmt.Printf("Val: %d", i)
-    }
+type Message struct {
+    Name string    `json:"name"`
 }
 
-func oddNumbers(c chan []string) {
-	for i := 1; i < 100; i = i + 2 {
-		c <- []string{strconv.Itoa(i), "odd"}
-		time.Sleep(800 * time.Millisecond)
-	}
-	close(c)
-}
-
-func evenNumbers(c chan []string) {
-	for i := 2; i < 100; i = i + 2 {
-		time.Sleep(400 * time.Millisecond)
-		c <- []string{strconv.Itoa(i), "even"}
-		time.Sleep(400 * time.Millisecond)
-	}
-	close(c)
+func router() *gin.Engine {
+    r := gin.Default()
+    r.GET("/:name&mne", func(c *gin.Context) {
+        user := c.Param("name&mne")
+        designation := c.Param("mne")
+        c.String(http.StatusOK, fmt.Sprintf("Hello %s, you are an %s", user, designation))
+    })
+    return r
 }
 
 func main() {
-	str := fmt.Sprintf("not \n")
-	fmt.Printf("::::::::::This is %s ::::::::::\n", str)
-	str = strings.TrimSpace(str)
-	fmt.Printf("::::::::::This is %s ::::::::::", str)
-	// ch := make(chan []string)
-	// go oddNumbers(ch)
-	// go evenNumbers(ch)
-	// fmt.Println(readinputs.ReadTxt("APIKEY"))
-	// go writeAPI()
-
-	// for rec := range ch {
-	// 	num, str := rec[0], rec[1]
-	// 	fmt.Printf("this is %v:{%v}\n", str, num)
-	// }
-
-	// for {
-	// 	select {
-	// 	case v := <-ch:
-	// 		fmt.Printf("This is V: %v \n", v)
-	// 	case <-time.After(300 * time.Millisecond):
-	// 		fmt.Println("Waited too long for response")
-	// 		os.Exit(0)
-	// 	}
-	// }
-
+    router().Run()
 }
